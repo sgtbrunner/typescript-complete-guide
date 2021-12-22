f7443596a2ade13990b4adf999df0f62b4f0b247
sgtbrunner <guilherme.brunner@gmail.com>
2021-12-22 15:20:27 -0300

    adds sort app implementation

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hi there');
+import CharactersCollection from './CharactersCollection';
+import LinkedList from './LinkedList';
+import NumbersCollection from './NumbersCollection';
+
+// CHARACTERS
+const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log('Characters:', charactersCollection.data);
+
+// LINKED LIST
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.print();
+
+// NUMBERS
+const numbersCollection = new NumbersCollection([10, 0, -5, 3]);
+numbersCollection.sort();
+console.log('Numbers:', numbersCollection.data);
